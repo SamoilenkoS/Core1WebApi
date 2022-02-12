afbe8967d157f61d8f98a2b4d0d950568cf4a7fc
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-02-12 12:54:05 +0200

    Add project files.

---
+++ Core1WebApi/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using Core1WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core1WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> _users;
+        private readonly ILogger<UsersController> _logger;
+
+        static UsersController()
+        {
+            _users = new List<User>();
+        }
+
+        public UsersController(
+            ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            user.Id = Guid.NewGuid();
+            _users.Add(user);
+
+            return Created(user.Id.ToString(), user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            User user = _users.FirstOrDefault(user => user.Id == id);
+            if(user != null)
+            {
+                return Ok(user);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateUser(User user)
+        {
+            var dbUser = _users.FirstOrDefault(x => x.Id == user.Id);
+            if(dbUser != null)
+            {
+                // _users
+                var index = _users.IndexOf(dbUser);
+                _users[index] = user;
+
+                return Ok(user);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(Guid id)
+        {
+            var dbUser = _users.FirstOrDefault(x => x.Id == id);
+
+            if (dbUser != null)
+            {
+                _users.Remove(dbUser);
+
+                return Ok(dbUser);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Core1WebApi/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core1WebApi.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
